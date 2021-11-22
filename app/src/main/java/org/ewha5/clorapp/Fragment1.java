71437f7f8150fceba9fb5be3f5d90984fdc5672c
Yunju J <amy9590@naver.com>
2021-11-22 23:23:59 +0900

    server_connection
    succeeded while failed

--- app/src/main/java/org/ewha5/clorapp/Fragment1.java
+++ app/src/main/java/org/ewha5/clorapp/Fragment1.java
@@ -2,16 +2,10 @@ package org.ewha5.clorapp;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
-import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Matrix;
-import android.graphics.Picture;
 import android.media.ExifInterface;
-import android.os.Environment;
 import android.os.Handler;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -19,6 +13,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -28,28 +23,48 @@ import android.widget.Toast;
 
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.content.FileProvider;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.util.Date;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/*윤주*/
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.entity.StringEntity;
+
+import java.net.URL;
+
+import android.util.Base64;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Fragment1 extends AppCompatActivity {
+    subThread subthread;
     private static final String TAG = "Fragment1";
 
-    Context context;
+    //Context context;
     ImageView pictureImageView;
-    boolean isPhotoCaptured;
+    //boolean isPhotoCaptured;
 
     Uri uri;
     File file;
-    Bitmap resultPhotoBitmap;
+    static Bitmap resultPhotoBitmap;
+
+    /*윤주*/
+    public static String URL = "http://146.56.137.184:5000/receive";
+
 
     //추가
     Handler handler = new Handler();
@@ -95,6 +110,10 @@ public class Fragment1 extends AppCompatActivity {
                 //웹 서버로 연결
                 request();
 
+
+
+
+
                 //프로그레스 바 - 서버 후 수정 필요
                 dialog = new ProgressDialog(Fragment1.this);
                 dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
@@ -114,12 +133,87 @@ public class Fragment1 extends AppCompatActivity {
     //서버로 연결
     private void request() {
         handler.postDelayed(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void run() {
+                if(Build.VERSION.SDK_INT > 8){
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                }
+
+                //subThread.sendHttpWithMsg(URL);
+
+                post(URL);
             }
             //서버 연결
-        }, 5000);
+        }, 3000);
     }
+
+    public void post(String url){
+
+        try{
+            JSONObject obj = new JSONObject();
+            try {
+                obj.put("image", getStringFromBitmap(resultPhotoBitmap));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            URL url1 = new URL(url);
+
+            HttpURLConnection conn = null;
+
+            conn = (HttpURLConnection) url1.openConnection();
+            Log.e(TAG, "1");
+            conn.setRequestProperty("Accept-Encoding", "identity");
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            Log.e(TAG, "2");
+            conn.setDoOutput(true);
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            Log.e(TAG, "3");
+            bufferedWriter.write(obj.toString());
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            Log.e(TAG, "SENT");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String returnMsg = in.readLine();
+            System.out.println("응답메시지:" + returnMsg);
+            
+            //응답코드 수신
+            int responseCode = conn.getResponseCode();
+            if(responseCode ==400){
+                System.out.println("400: 실행오류");
+            } else if (responseCode ==500){
+                System.out.println("500: 서버오류");
+            } else{
+                System.out.println(responseCode+":응답코드");
+            }
+
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+    }
+
+    public static String getStringFromBitmap(Bitmap bitmapPicture) {
+        String encodedImage;
+        ByteArrayOutputStream byteArrayBitmapStream = new ByteArrayOutputStream();
+        bitmapPicture.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayBitmapStream);
+        byte[] b = byteArrayBitmapStream.toByteArray();
+        encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+        Log.e(TAG, "0");
+        return encodedImage;
+    }
+
+
 
     //서버 연결 후, 다음 화면 넘어가기 위한 메시지
     private void showMessage() {
@@ -237,6 +331,7 @@ public class Fragment1 extends AppCompatActivity {
 
                     //setPicture(file.getAbsolutePath(), 8);
                     resultPhotoBitmap = decodeSampledBitmapFromResource(file, pictureImageView.getWidth(), pictureImageView.getHeight());
+                    /*윤주*/ //  이미지 -> 바이트 변환 -> Base64 인코딩 -> Json value 전달
                     pictureImageView.setImageBitmap(resultPhotoBitmap);
 
                     /*
@@ -263,9 +358,10 @@ public class Fragment1 extends AppCompatActivity {
 
                         setImage();
                     }
-                    
+
                      */
 
+                    Toast.makeText(getApplicationContext(), "사진 업로드에 성공했습니다!", Toast.LENGTH_SHORT).show();
                     break;
 
                 case AppConstants.REQ_PHOTO_SELECTION:  // 사진을 앨범에서 선택하는 경우
@@ -340,7 +436,7 @@ public class Fragment1 extends AppCompatActivity {
 
                         setImage();
                     }
-
+                    Toast.makeText(getApplicationContext(), "사진 업로드에 성공했습니다!", Toast.LENGTH_SHORT).show();
                     break;
 
             }
---
+++ app/src/main/java/org/ewha5/clorapp/subThread.java
@@ -0,0 +1,71 @@
+package org.ewha5.clorapp;
+
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpEntity;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpResponse;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.ClientProtocolException;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.methods.HttpPost;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.utils.URIBuilder;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.entity.StringEntity;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.impl.client.DefaultHttpClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+/*윤주*/
+// 서버로 업로드
+
+
+public class subThread extends Thread {
+    public void sendHttpWithMsg(String url) {
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(url);
+        URIBuilder newBuilder = new URIBuilder(post.getURI());
+        post.setHeader("Content-type", "application/json; charset=utf-8");
+
+        //Json파일 만들기
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("image", Fragment1.getStringFromBitmap(Fragment1.resultPhotoBitmap));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            StringEntity se;
+            se = new StringEntity(obj.toString());
+            HttpEntity he = se;
+            post.setEntity(he);
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+
+        String line = null;
+        String result = "";
+
+        try {
+            HttpResponse response = client.execute(post);   // 문제
+            BufferedReader bufReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "utf-8"));
+            while ((line = bufReader.readLine()) != null) {
+                result += line;
+            }
+        } catch (ClientProtocolException protocolException) {
+            protocolException.printStackTrace();
+            //return "Error"+protocolException.toString();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            //return "Error"+ioException.toString();
+        }
+
+        //return result;
+
+
+    }
+
+
+}
